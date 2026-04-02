67a9c186025e0faeb48622681bb3056fa8265704
Paul <polo13410@hotmail.fr>
2026-04-02 18:45:12 +0200

    Restart

--- front/src/api/index.ts
+++
@@ -1,9 +0,0 @@
-
-// Main API exports
-export * from './config/axios';
-export * from './hooks/useApi';
-export * from './utils/errorHandling';
-
-// Endpoint exports
-export * from './endpoints/appointments';
-export * from './endpoints/patients';
--- infrastructure/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cloudfront_distribution_vp_front_app" {
-  value       = aws_cloudfront_distribution.vp_front_app.id
-  description = "The ID of the CloudFront distribution for the frontend app"
-}
-
-output "cloudfront_domain_name_vp_front_app" {
-  value       = aws_cloudfront_distribution.vp_front_app.domain_name
-  description = "The domain name of the CloudFront distribution for the frontend app"
-}
